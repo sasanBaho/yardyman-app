3a257deb6af7a07aab63c755f56b054ebc349957
sasanBaho <baho.sasan@gmail.com>
2026-04-27 09:55:02 -0400

    Move Firebase config to environment variables

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkEei5sDldeWlMmXB852m_pheh96zF048",
-  authDomain: "lawn-snow-6ab68.firebaseapp.com",
-  projectId: "lawn-snow-6ab68",
-  storageBucket: "lawn-snow-6ab68.firebasestorage.app",
-  messagingSenderId: "894448553102",
-  appId: "1:894448553102:web:8466eed52ebb1e10e48ddf",
-  measurementId: "G-QP14130PHF"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
